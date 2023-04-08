18803d411e09a632a823fab92afdfd25118f0318
Wochozka <david@svarc.it>
2023-04-08 10:54:32 +0200

    add shebang line for /usr/bin/env python3 move imports to the top of file remove redundant parenthesis on each return line add second blank lines before each function (there should be two)

--- ak.py
+++ ak.py
@@ -1,33 +1,42 @@
+#!/usr/bin/env python3
+
+import random
+import string
+
 import pandas
 data = pandas.read_csv(" ")
+
 
 def jmeno(jmeno_a_prijmeni: str):
     rozdelene_jmeno = jmeno_a_prijmeni.split()
     if len(rozdelene_jmeno) == 2:
-        return(rozdelene_jmeno[0])
+        return rozdelene_jmeno[0]
     elif len(rozdelene_jmeno) == 3:
-        return(f"{rozdelene_jmeno[0]} {rozdelene_jmeno[1]}")
+        return f"{rozdelene_jmeno[0]} {rozdelene_jmeno[1]}"
+
 
 def prijmeni(jmeno_a_prijmeni: str):
     rozdelene_jmeno = jmeno_a_prijmeni.split()
     if len(rozdelene_jmeno) == 2:
-        return(rozdelene_jmeno[1])
+        return rozdelene_jmeno[1]
     elif len(rozdelene_jmeno) == 3:
-        return(rozdelene_jmeno[2])
+        return rozdelene_jmeno[2]
+
 
 def firemni_email(jmeno_a_prijmeni: str):
     rozdelene_jmeno = jmeno_a_prijmeni.split()
     if len(rozdelene_jmeno) == 2:
-        return(f"{rozdelene_jmeno[0]}.{rozdelene_jmeno[1]}@domena.pripona")
+        return f"{rozdelene_jmeno[0]}.{rozdelene_jmeno[1]}@domena.pripona"
     elif len(rozdelene_jmeno) == 3:
-        return(f"{rozdelene_jmeno[0]}-{rozdelene_jmeno[1]}.{rozdelene_jmeno[2]}@domena.pripona")
+        return f"{rozdelene_jmeno[0]}-{rozdelene_jmeno[1]}.{rozdelene_jmeno[2]}@domena.pripona"
+
 
 def uzivatelske_jmeno(jmeno_a_prijmeni: str):
     rozdelene_jmeno = jmeno_a_prijmeni.split()
     if len(rozdelene_jmeno) == 2:
-        return(f"{rozdelene_jmeno[0]}.{rozdelene_jmeno[1]}")
+        return f"{rozdelene_jmeno[0]}.{rozdelene_jmeno[1]}"
     elif len(rozdelene_jmeno) == 3:
-        return(f"{rozdelene_jmeno[0]}-{rozdelene_jmeno[1]}.{rozdelene_jmeno[2]}")
+        return f"{rozdelene_jmeno[0]}-{rozdelene_jmeno[1]}.{rozdelene_jmeno[2]}"
 
 
 data["Jméno"] = data["Jméno a příjmení"].apply(jmeno)
@@ -35,8 +44,6 @@ data["Příjmení"] = data["Jméno a příjmení"].apply(prijmeni)
 data["Firemní e-mail"] = data["Jméno a příjmení"].apply(firemni_email)
 data["Uživatelské jméno"] = data["Jméno a příjmení"].apply(uzivatelske_jmeno)
 
-import random
-import string
 
 def heslo(delka=10):
     pismena = string.ascii_letters + string.digits
